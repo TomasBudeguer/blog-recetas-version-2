d21943c22f96b083d07433819b1236be2707de1b
Tomas <tomibudeguer20@gmail.com>
2022-10-14 15:20:41 -0300

    hice el componente administrador con la table y el componente item con el tr de la tabla

--- src/components/views/Administrador.jsx
+++ src/components/views/Administrador.jsx
@@ -1,9 +1,38 @@
+import { Container, Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import ItemReceta from "./receta/ItemReceta";
+
 const Administrador = () => {
-    return (
-        <div>
-            administrador
-        </div>
-    );
+  return (
+    <Container className="my-5 mainSection">
+      <div className="d-flex justify-content-between align-items-center">
+        <aside>
+          <h2 className="display-4">Prouctos disponibles</h2>
+        </aside>
+        <aside>
+          <Link className="btn btn-primary" to="/administrar/crear">
+            Agregar
+          </Link>
+        </aside>
+      </div>
+      <hr />
+      <Table striped bordered hover responsive>
+        <thead className="bg-success text-white">
+          <tr>
+            <th>Cod</th>
+            <th>Producto</th>
+            <th>Descripcion</th>
+            <th>URL de Imagen</th>
+            <th>Categoria</th>
+            <th>Opciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          <ItemReceta></ItemReceta>
+        </tbody>
+      </Table>
+    </Container>
+  );
 };
 
 export default Administrador;
--- src/components/views/receta/ItemReceta.jsx
+++ src/components/views/receta/ItemReceta.jsx
@@ -1,9 +1,20 @@
+import { Button } from "react-bootstrap";
 const ItemReceta = () => {
-    return (
-        <div>
-            item receta
-        </div>
-    );
+  return (
+    <tr>
+      <td>1</td>
+      <td>Nombre receta</td>
+      <td>Descripcion</td>
+      <td>URL</td>
+      <td>Categoria</td>
+      <td>
+        <Button className="btn btn-success me-2 mb-1">Editar</Button>
+        <Button variant="danger" className="mb-1">
+          Borrar
+        </Button>
+      </td>
+    </tr>
+  );
 };
 
 export default ItemReceta;

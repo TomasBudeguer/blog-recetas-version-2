cc9cb795e8909b7d3797fd19d27d64b50341a6de
Tomas <tomibudeguer20@gmail.com>
2022-10-19 19:56:18 -0300

    hice la peticion para obtener la receta y mostrarla en el componente detalle con cada una de ellas

--- src/components/views/DetalleReceta.jsx
+++ src/components/views/DetalleReceta.jsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
 import { Card, Badge, Row, Col, Container } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import Swal from "sweetalert2";
+import { obtenerRecetaAPI } from "../helpers/queries";
+
 
 const DetalleReceta = () => {
+  const {id} = useParams()
+  const [nombreReceta, setNombreReceta] = useState("")
+  const [descripcion, setDescripcion] = useState("")
+  const [imagen, setImagen] = useState("");
+  const [categoria, setCategoria] = useState("");
+
+  useEffect(()=>{
+    obtenerRecetaAPI(id).then((respuesta)=>{
+      if(respuesta.status === 200){
+        setNombreReceta(respuesta.dato.nombreReceta)
+        setDescripcion(respuesta.dato.descripcion)
+        setImagen(respuesta.dato.imagen)
+        setCategoria(respuesta.dato.categoria)
+      }else {
+        Swal.fire(
+          "Ocurrio un error",
+          "Intente este paso en unos minutos",
+          "error"
+        );
+      }
+    })
+  })
+
+
   return (
     <Container className="my-4 mainSection">
       <Card>
@@ -8,21 +37,22 @@ const DetalleReceta = () => {
           <Row>
             <Col sm={12} md={4}>
               <img
-                src="https://images.pexels.com/photos/4033636/pexels-photo-4033636.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                alt="receta"
+                src={imagen}
+                alt={nombreReceta}
                 className="w-100"
               />
             </Col>
             <Col sm={12} md={8}>
               <Card.Title className="mt-1">
-                <h3>Nombre Receta</h3>
+                <h3>{nombreReceta}</h3>
               </Card.Title>
+              <hr />
               <div className="d-flex flex-column">
                 <aside>
-                  <Badge bg="success">Categoria</Badge>
+                  <Badge bg="success">{categoria}</Badge>
                 </aside>
                 <aside>
-                  <Card.Text>Descripcion</Card.Text>
+                  <Card.Text>{descripcion}</Card.Text>
                 </aside>
               </div>
             </Col>
